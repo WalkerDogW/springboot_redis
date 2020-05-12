3c700587f47869e3fd459343e6f4e55780f6c34f
WalkerDogW <WalkerDogW@outlook.com>
2020-05-12 16:39:08 +0800

    Initial commit

---
+++ src/main/java/site/javaee/springboot_redis/controller/HelloController.java
@@ -0,0 +1,27 @@
+package site.javaee.springboot_redis.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+import site.javaee.springboot_redis.pojo.User;
+
+
+/**
+ * @author Tao
+ * @create 2020/4/23 20:27
+ */
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+
+    //只要接口返回值中存在实体类，就会被扫描到swagger的models
+    @PostMapping("/user/{username}")
+    @ApiOperation("新增hello用户")
+    public User user(@ApiParam("用户名") @PathVariable("username") String username){
+        return  new User(username,"123456");
+    }
+}
---
+++ src/test/java/site/javaee/springboot_redis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package site.javaee.springboot_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
